302f58662c9dd46b9d09c17a0318fe36948e8268
Gwendal Le Squer <gwendal.le-squer@hotmail.fr>
2015-02-22 14:48:33 +0100

    Base de Salotti
    Programme de base donné par Salotti

---
+++ taquin/projettaquin/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projettaquin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graph g = new Graph();
+            NodeTaquin N0 = new NodeTaquin(textBox1.Text);
+            List<GenericNode> Lres = g.RechercheSolutionAEtoile(N0);
+
+            if (Lres.Count == 0)
+            {
+                labelsolution.Text = "Pas de solution";
+            }
+            else
+            {   labelsolution.Text = "Une solution a été trouvée";
+                foreach (GenericNode N in Lres)
+                {
+                    listBox1.Items.Add( N);
+                }
+                labelcountopen.Text = "Nb noeuds des ouverts : " + g.CountInOpenList().ToString();
+                labelcountclosed.Text = "Nb noeuds des fermés : "+ g.CountInClosedList().ToString();
+                g.GetSearchTree( treeView1 );
+            }
+
+        }
+    }
+}
---
+++ taquin/projettaquin/GenericNode.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projettaquin
+{
+    // classe abstraite, il est donc impératif de créer une classe qui en hérite
+    // pour résoudre un problème particulier en y ajoutant des infos liées au contexte du problème
+    abstract public class GenericNode
+    {
+        protected string Name;              // DOIT ETRE UNIQUE POUR CHAQUE genericnode !!
+        protected double GCost;               //coût du chemin du noeud initial jusqu'à ce noeud
+        protected double HCost;               //estimation heuristique du coût pour atteindre le noeud final
+        protected double TotalCost;           //coût total (g+h)
+        protected GenericNode ParentNode;     // noeud parent
+        protected List<GenericNode> Enfants;  // noeuds enfants
+
+        public GenericNode(string nom)
+        {
+            Name = nom;
+            ParentNode = null;
+            Enfants = new List<GenericNode>();
+        }
+
+        public string GetNom()
+        {
+            return Name;
+        }
+
+        public double GetGCost()
+        {
+            return GCost;
+        }
+
+        public void SetGCost(double g)
+        {
+            GCost = g;
+        }
+
+        public double Estimation()
+        {
+            return HCost;
+        }
+
+        public void SetEstimation(double h)
+        {
+            HCost = h;
+        }
+
+
+        public double Cout_Total
+        {
+            get { return TotalCost; }
+            set { TotalCost = value; }
+        }
+
+        public List<GenericNode> GetEnfants()
+        {
+            return Enfants;
+        }
+
+        public GenericNode GetNoeud_Parent()
+        {
+            return ParentNode;
+        }
+
+        public void SetNoeud_Parent(GenericNode g)
+        {
+            ParentNode = g;
+            g.Enfants.Add(this);
+        }
+
+        public void Supprime_Liens_Parent()
+        {
+            if (ParentNode == null) return;
+            ParentNode.Enfants.Remove(this);
+            ParentNode = null;
+        }
+
+        public void calculCoutTotal()
+        {
+            TotalCost = GCost + HCost;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        // Méthodes abstrates, donc à surcharger obligatoirement avec override dans une classe fille
+        public abstract double GetArcCost(GenericNode N2);
+        public abstract bool EndState();
+        public abstract List<GenericNode> GetListSucc();
+        public abstract void CalculeHCost();
+    }
+}
